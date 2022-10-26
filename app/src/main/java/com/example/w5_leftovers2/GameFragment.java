b3172b991a7a9e1230683c203ea7ceef74f1fd48
Lauren Pearson <raspberrylauren@gmail.com>
2022-10-25 20:58:46 -0400

    Initial commit

---
+++ app/src/main/java/com/example/w5_leftovers2/GameFragment.java
@@ -0,0 +1,97 @@
+package com.example.w5_leftovers2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Random;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GameFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+
+public class GameFragment extends Fragment {
+    Button bn00, bn01, bn02, bn03,
+            bn10, bn11, bn12, bn13,
+            bn20, bn21, bn22, bn23,
+            bn30, bn31, bn32, bn33;
+    private Object findViewByID;
+
+
+    public String getRandomLetter(){
+        Random rnd = new Random();
+        char c = (char) ('a' + rnd.nextInt(26));
+        Log.i("CHAR", "getRandomLetter: " + c);
+        String res = "" + c;
+        return res;
+    }
+
+
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public GameFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment GameFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static GameFragment newInstance(String param1, String param2) {
+        GameFragment fragment = new GameFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.extra, container, false);
+        bn00 = (Button) view.findViewByID(R.id.button00);
+        return view;
+
+
+
+        //return inflater.inflate(R.layout.fragment_game, container, false);
+
+    }
+}
